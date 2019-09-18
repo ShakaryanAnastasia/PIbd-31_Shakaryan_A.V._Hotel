1c7d6c88970e608f477245b62593a67321d10f19
Anastasia <Nastya.Shakaryan@yandex.ru>
2019-09-18 12:57:57 +0400

    first deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const forceSSL = function() {
+return function (req, res, next) {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		return res.redirect(
+			['https://', req.get('Host'), req.url].join('')
+			);
+		}
+	next();
+	}
+}
+app.use(forceSSL());
+app.use(express.static('PIbd-31_Shakaryan_A.V._Hotel/dist'));
+app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+res.sendFile(path.join('PIbd-31_Shakaryan_A.V._Hotel/dist/Hotel/index.html'));
+});
